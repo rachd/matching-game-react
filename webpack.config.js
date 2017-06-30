ffa33398ddeb2a40abe02e58f1d8e5faf66fef74
rachd <rachel.m.dorn@gmail.com>
2017-06-30 15:34:57 -0400

    add react

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
 module.exports = {
-  entry: './client/index.js',
+  entry: './app/index.js',
   output: {
     path: path.resolve('dist'),
     filename: 'index_bundle.js'
@@ -10,5 +16,6 @@ module.exports = {
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig]
 }
